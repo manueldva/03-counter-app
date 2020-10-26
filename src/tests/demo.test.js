18ef3c6da54bbdd506d9db0f14095cd31a2e19ca
Manuel David Avila <mavila@mdh.gob.ar>
2020-10-26 16:41:45 -0300

    test

---
+++ src/tests/demo.test.js
@@ -0,0 +1,23 @@
+
+
+describe('Pruebas en el archivo demo.test.js', () => {
+    
+    test('debe se ser iguales los string', () => {
+    
+        // 1. inicializacion
+        const mensaje = 'Hola Mundo';
+        
+        //2. estimulo 
+        const mensaje2 = `Hola Mundo`;
+    
+        //3. observar el comportamiento
+    
+        expect( mensaje ).toBe( mensaje2 );
+        
+    })
+
+})
+
+
+
+
